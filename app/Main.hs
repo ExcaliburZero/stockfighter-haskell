e29db597e233795de41582e4e750f799c98cd482
Christopher Wells <cwellsny@nycap.rr.com>
2016-06-09 19:10:54 -0400

    Move library into subdirectories
    Move the library contents into the proper subdirectories. Also update the appliction import and build config file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Network.Stockfighter.Lib
 
 main :: IO ()
 main = someFunc
--- src/Network/Stockfighter/Lib.hs
+++ src/Network/Stockfighter/Lib.hs
@@ -1,4 +1,4 @@
-module Lib
+module Network.Stockfighter.Lib
     ( someFunc
     ) where
 
